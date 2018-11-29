5b06b8c24350e677d819c44f656deb8545210e5b
bxguo <951409002@qq.com>
2018-11-29 16:25:53 +0800

    exc: Ordering排序

---
+++ guava-tests/test/com/google/common/base/OrderingTest.java
@@ -0,0 +1,59 @@
+package com.google.common.base;
+
+import com.google.common.collect.Ordering;
+import com.google.common.primitives.Ints;
+import com.google.common.testing.NullPointerTester;
+import junit.framework.TestCase;
+
+import javax.annotation.Nullable;
+
+/**
+ * @author: bxguo
+ * @time: 2018/11/29 14:43
+ */
+public class OrderingTest extends TestCase {
+    public void testCreatOrdering() {
+
+        //排序其创建
+        Ordering.natural();
+        Ordering.usingToString();
+        new Ordering<String>() {
+            public int compare(String left, String right) {
+                return Ints.compare(left.length(), right.length());
+            }
+        };
+
+        //排序规则 TODO 获取迭代器
+        Ordering<Iterable<Comparable>> lexicographical = Ordering.natural().lexicographical();
+
+        //获取两个里面最大值
+        Function<Foo, Integer> function = new Function<Foo, Integer>() {
+            public Integer apply(Foo foo) {
+                return foo.notSortedBy;
+            }
+        };
+        Ordering<Foo> ordering = Ordering.natural().nullsFirst().onResultOf(function);
+        Foo max = ordering.max(new Foo("10", 10), new Foo("9", 9));
+        assertEquals(max.toString(), "Foo{sortedBy='10', notSortedBy=10}");
+    }
+
+
+    class Foo {
+        @Nullable
+        String sortedBy;
+        int notSortedBy;
+
+        public Foo(@Nullable String sortedBy, int notSortedBy) {
+            this.sortedBy = sortedBy;
+            this.notSortedBy = notSortedBy;
+        }
+
+        @Override
+        public String toString() {
+            return "Foo{" +
+                    "sortedBy='" + sortedBy + '\'' +
+                    ", notSortedBy=" + notSortedBy +
+                    '}';
+        }
+    }
+}
